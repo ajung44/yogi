736daa45860c6f624cf5e14b131eed13b1ace224
ajung44 <andrewjung89@icloud.com>
2023-02-11 13:05:46 -0500

    Added pictures

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import picture from "../public/picture.png"
 export default function Item(props) {
   const {
     ide, name, price, accompaniment, description, hot, extra, pic
@@ -8,13 +8,17 @@ export default function Item(props) {
   return (
     <div className="menuItem" identifier={ide}>
       <div className="name" id={hot ? "hot" : "notHot"}><span className="inlineId">{ide}. </span>{name}</div>
-      {pic && <Image className="foodThumb" src={require(`../public/${pic}`)} alt={name}/>}
+      
       <div className="details">
         { accompaniment && <div className="accompaniment">Comes with {accompaniment}</div>}
         <div className="description">{description}</div>
         <div className="extra">{extra}</div>
       </div>
-      <div className="price">${price}</div>
+      <div className="price">
+        {pic && <Image className="menuPic" src={picture} alt="view picture" />}
+        ${price}
+      </div>
+      {pic && <Image className="foodThumb" src={require(`../public/${pic}`)} alt={name}/>}
     </div>
   )
 };
