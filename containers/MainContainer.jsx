74dedec2746a0eee79f8ccf79891fd233b811e22
ajung44 <andrewjung89@icloud.com>
2023-02-11 04:41:41 -0500

    fluff

--- containers/MainContainer.jsx
+++ containers/MainContainer.jsx
@@ -6,6 +6,7 @@ export default function MainContainer(props) {
   return(
     <div className="mainContainer" ref={props.topRef}>
       <Image className="foodBG" src={bg} alt="SuwaneeBG"/>
+      <section>Welcome to <span className="yogi">yogi</span></section>
       <Image className="foodPoster" src={food} alt="Korean Atlanta dinner picture"/>
       <h1 className="fluff">We serve the modern Asian cuisine you deserve</h1>
     </div>
