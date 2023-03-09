874d70e7c82cffe4bffeed471f22416362b3e2ab
ajung44 <andrewjung89@icloud.com>
2023-03-08 20:41:27 -0500

    Changed business hours

--- containers/AboutContainer.jsx
+++ containers/AboutContainer.jsx
@@ -23,13 +23,13 @@ export default function AboutContainer(props) {
           <h2>
             Our Business Hours:
           </h2>
-          Monday:<br/>11:00AM - 8:00PM<br/><br/>
+          Monday:<br/>10:30AM - 8:00PM<br/><br/>
           <span className="closed">Tuesday:<br/>CLOSED</span><br/><br/>
-          Wednesday:<br/>11:00AM - 8:00PM<br/><br/>
-          Thursday:<br/>11:00AM - 8:00PM<br/><br/>
-          Friday:<br/>11:00AM - 9:00PM<br/><br/>
-          Saturday:<br/>11:00AM - 9:00PM<br/><br/>
-          Sunday:<br/>11:00AM - 8:00PM<br/><br/>
+          Wednesday:<br/>10:30AM - 8:00PM<br/><br/>
+          Thursday:<br/>10:30AM - 8:00PM<br/><br/>
+          Friday:<br/>10:30AM - 9:00PM<br/><br/>
+          Saturday:<br/>10:30AM - 9:00PM<br/><br/>
+          Sunday:<br/>10:30AM - 8:00PM<br/><br/>
 
           <h3>
             Last order at closing time.
