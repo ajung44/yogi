f85c86980fdebc1f421c3b2b272091691e750ae5
Andrew Jung <andrewjung89@icloud.com>
2024-02-26 14:29:33 -0500

    Change speed insight from nextjs to react

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function MyApp({ Component, pageProps }) {
   return(
