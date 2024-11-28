ff21c0d34f439c9703dc3839abd6b4638eee2217
Andrew Jung <andrewjung89@icloud.com>
2024-11-27 22:31:16 -0500

    SEO Optimization

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import logo from '../src/assets/logo.png';
 import logoSmall from '../src/assets/logo-small.png';
 import UsContainer from '../containers/UsContainer';
 import GalleryContainer from '../containers/GalleryContainer';
+import { Restaurant, WithContext } from 'schema-dts';
 
 export default function Home() {
   const [currCat, setCat] = useState("donburi");
@@ -21,6 +22,42 @@ export default function Home() {
   const aboutRef = useRef(null);
 
   const category = new Map();
+
+  const schema = {
+    '@context': 'https://schema.org',
+    '@type': 'Restaurant',
+    name: 'Yogi',
+    alternateName: "요기",
+    description: "The best Korean food in Suwanee",
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: '80 Horizon Dr STE 201',
+      addressLocality: 'Suwanee',
+      addressRegion: 'GA',
+      postalCode: '30024',
+      addressCountry: 'US',
+    },
+    telephone: '678-926-3019',
+    servesCuisine: ['Korean', 'Asian'],
+    priceRange: '$',
+    openingHours: [
+      'Mo 10:30-21:00',
+      'Tu Closed',
+      'We 10:30-21:00',
+      'Th 10:30-21:00',
+      'Fr 10:30-21:00',
+      'Sa 10:30-21:00',
+      'Su 11:00-20:00',
+    ],
+    geo: {
+      '@type': 'GeoCoordinates',
+      latitude: 34.0292062,
+      longitude: -84.043746,
+    },
+    url: 'https://yogiatl.com',
+    menu: 'https://www.clover.com/online-ordering/yogi-suwanee',
+    acceptsReservations: 'No',
+  };
 
   function handleScroll() {
     const position = window.pageYOffset;
@@ -57,12 +94,17 @@ export default function Home() {
         window.removeEventListener('scroll', handleScroll);
     };
   },[])
+
   return (
     <>
       <Head >
         <title>Yogi - Korean American Restaurant in Suwanee, Georgia</title>
         <meta name="description" content="Welcome to Yogi, where you'll find familiar Korean dishes like Kimbap (김밥), Tteokbokki (떡볶이) to KBBQ experience like Bulgogi (불고기), Pork Belly (삼겹살), and more. Yogi is the perfect place for lunch or dinner with friends and family, and we always use the freshest ingredients available." />
         <link rel="icon" href="/logo-small.png" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
       </Head>
       <div className={styles.container}>
         <main>
